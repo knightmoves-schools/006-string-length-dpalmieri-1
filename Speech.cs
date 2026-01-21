e4b66b2f57c69993bb3326f6fec6d4c6099d045a
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-01-21 10:30:08 -0600

    Update Speech.cs

--- Speech.cs
+++ Speech.cs
@@ -1,15 +1,17 @@
 ﻿namespace knightmoves;
 public class Speech
 {   
-    public const string Quote = "...ask not what your country can do for you — ask what you can do for your country.";
+    public const string Quote = @"...ask not what your country can do for you — ask what you can do for your country.";
 
     public string PrintLengthAndTwentySecondLetter(){
         //Add your code here
-        public static int QuoteLength = Quote.Length;
+        public static int Length = Quote.Length;
         public char TwentySecondLetter = Quote[21];
-        return TwentySecondLetter + " " + Quote.Length;
+        public string solved = $@"{Length + TwentySecondLetter}";
+        return solved;
     }
 }
+
 
 
 
